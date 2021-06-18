0946ffaefcd17ff09300992caf69bb38285e3a5f
e-guyot <56032211+e-guyot@users.noreply.github.com>
2021-06-18 15:48:39 +0200

    add homapage

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -3,5 +3,27 @@ import 'package:flutter/material.dart';
 class AddContactPage extends StatelessWidget {
 
   @override 
-  widget build()
+  widget build(BuildContext context()) {
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 65,
+                height: 65,
+                color: Colors.red,
+              ),
+              TextFormField(),
+            ],
+          )
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons, save),
+        ),
+    );
+  }
 }
